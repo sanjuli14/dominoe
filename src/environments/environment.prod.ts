639601e9fa612526318ca79a9e7b550ab3fed4f9
sanjuli14 <juliocesargarciagarcia395@gmail.com>
2026-04-16 19:47:13 -0400

    clave correcta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://nqvoybdinpvdwmledfgf.supabase.co',
-  supabaseAnonKey: 'sb_publishable_7_TVwSWb2Qfpjvjn6jxHhQ_xszxaluG',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xdm95YmRpbnB2ZHdtbGVkZmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTg0NTQsImV4cCI6MjA5MTczNDQ1NH0.AVUTFLMiACFyjcui7wTtGn_7KJAF09WvgUQOcfh-BjY',
 };
